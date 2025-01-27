118dbcdba62885a5eb3d602e1d14c0fb04c95ce9
Eduardo Ruiz <ruizzmarin20@gmail.com>
2025-01-27 20:29:13 -0300

    first commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Esta es mi primera app hecha desde cero !!!!!!!</Text>
       <StatusBar style="auto" />
     </View>
   );
