ff3bdf56f36f6220fb7e37e5f512118a97b6390f
Eduardo Ruiz <ruizzmarin20@gmail.com>
2025-01-28 16:48:25 -0300

    probando react native paper

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Image } from 'react-native';
+import { Button, Card, Text, TextInput } from "react-native-paper";
+
+const icon = require('./assets/icon.png')
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Esta es mi primera app hecha desde cero !!!!!!!</Text>
-      <StatusBar style="auto" />
+      <Card style={styles.card}>
+        <Card.Title title="Bienvenido" />
+        <Card.Content>
+          <Text>Esta es la primera vez que estoy usando react-native-paper.</Text>
+          <TextInput label="Escribe algo" style={styles.input} />
+        </Card.Content>
+        <Card.Actions>
+          <Button mode="contained" onPress={() => console.log("¡Hola!")}>
+            ¡Presiona acá!
+          </Button>
+        </Card.Actions>
+      </Card>
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 20,
+  },
+  card: {
+    width: "100%",
+    padding: 10,
+  },
+  input: {
+    marginBottom: 10,
   },
 });
